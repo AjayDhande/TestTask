63a8642870de5ba657eed7159c5fe9d6cd0ac5c2
Mayuri Kanholkar <mayuri.kanholkar@cryptextechnologies.com>
2018-06-22 19:02:58 +0530

    changes in js file.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,69 +1,12 @@
-function photo_one(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-
-      reader.onload = function (e) {
-        $('#img_prev1')
-          .attr('src', e.target.result)
-          .addClass('.display')
-          .width(252);
-      };
-
-      reader.readAsDataURL(input.files[0]);
-    }
+function photo_preview(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $('#img_prev_'+input.id.split("_")[3])
+        .attr('src', e.target.result)
+        .addClass('display')
+        .width(252);
+    };
+    reader.readAsDataURL(input.files[0]);
   }
-  function photo_two(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-
-      reader.onload = function (e) {
-        $('#img_prev2')
-          .attr('src', e.target.result)
-          .addClass('.display')
-          .width(252);
-      };
-
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
-  function photo_three(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-
-      reader.onload = function (e) {
-        $('#img_prev3')
-          .attr('src', e.target.result)
-          .addClass('.display')
-          .width(252);
-      };
-
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
-  function photo_four(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-
-      reader.onload = function (e) {
-        $('#img_prev4')
-          .attr('src', e.target.result)
-          .addClass('.display')
-          .width(252);
-      };
-
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
-  function photo_five(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-
-      reader.onload = function (e) {
-        $('#img_prev5')
-          .attr('src', e.target.result)
-          .addClass('.display')
-          .width(252);
-      };
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
+}
